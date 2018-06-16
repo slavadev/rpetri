4e063372da8b18ad562f327a5bdb71d2fefba943
Slava Korolev <korolvs@gmail.com>
2018-06-16 18:24:46 +0300

    Rewrited Arc model

--- lib/rpetri/arc.rb
+++ lib/rpetri/arc.rb
@@ -1,8 +1,26 @@
 module RPetri
   class Arc < Object
-    def initialize
-      @type = :arc
-      super
+    attr_reader :source, :target
+
+    def initialize(source, target)
+      @source = source
+      @target = target
+      validate!
+      super()
+    end
+
+    def type; :arc; end
+
+    protected
+
+    def validate!
+      unless @source.is_a?(Place) || @source.is_a?(Transition)
+        raise ValidationError, 'Source should be Place or Transition'
+      end
+      unless @target.is_a?(Place) || @target.is_a?(Transition)
+        raise ValidationError, 'Target should be Place or Transition'
+      end
+      raise ValidationError, 'Source and Target should be different type' if @source.type == @target.type
     end
   end
 end
--- spec/rpetri/arc_spec.rb
+++ spec/rpetri/arc_spec.rb
@@ -1,8 +1,55 @@
+require 'pry'
 RSpec.describe RPetri::Arc do
   describe 'initialize' do
-    let(:arc) { RPetri::Arc.new }
-    it 'sets arc type' do
-      expect(arc.type).to eq(:arc)
+    let(:place) { RPetri::Place.new }
+    let(:transition) { RPetri::Transition.new }
+    let(:source) { place }
+    let(:target) { transition}
+    subject { RPetri::Arc.new(source, target) }
+
+    context 'when correct arc' do
+      it 'does not raise and exception' do
+        expect{ subject }.not_to raise_error
+      end
+      it 'sets correct source and target' do
+        expect(subject.source).to eq(source)
+        expect(subject.target).to eq(target)
+      end
+    end
+
+    context 'when source is nil' do
+      let(:source) { nil }
+      it 'raises and exception' do
+        expect{ subject }.to raise_error(RPetri::ValidationError, 'Source should be Place or Transition')
+      end
+    end
+
+    context 'when source is wrong type' do
+      let(:source) { "source" }
+      it 'raises and exception' do
+        expect{ subject }.to raise_error(RPetri::ValidationError, 'Source should be Place or Transition')
+      end
+    end
+
+    context 'when target is nil' do
+      let(:target) { nil }
+      it 'raises and exception' do
+        expect{ subject }.to raise_error(RPetri::ValidationError, 'Target should be Place or Transition')
+      end
+    end
+
+    context 'when target is wrong type' do
+      let(:target) { "taget" }
+      it 'raises and exception' do
+        expect{ subject }.to raise_error(RPetri::ValidationError, 'Target should be Place or Transition')
+      end
+    end
+
+    context 'when source and target are the same type' do
+      let(:target) { place }
+      it 'raises and exception' do
+        expect{ subject }.to raise_error(RPetri::ValidationError, 'Source and Target should be different type')
+      end
     end
   end
 end
