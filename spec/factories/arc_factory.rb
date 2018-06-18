624c9cd1219194f8ab68452d0df8cad7f8de034c
Slava Korolev <korolvs@gmail.com>
2018-06-19 00:47:20 +0300

    Fixed Arc factory

--- spec/factories/arc_factory.rb
+++ spec/factories/arc_factory.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :arc, class: RPetri::Arc do
     initialize_with { new(source, target, options) }
-    source { place }
-    target { transition }
+    source { build :place }
+    target { build :transition }
     options Hash.new
   end
 end
