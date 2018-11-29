ba1beb36b98a389de6210eddaf94e3d6034506f3
korolvs <korolvs@gmail.com>
2018-11-29 13:55:57 +0100

    Fixed logger tabulation

--- lib/rpetri/extentions/net_place.rb
+++ lib/rpetri/extentions/net_place.rb
@@ -8,7 +8,7 @@ module RPetri
     def run(options = {})
       return unless @net
       nested_options = options.dup
-      nested_options[:logger_prefix] = nested_options[:logger_prefix] + '  '
+      nested_options[:logger_prefix] += '  '
       @net.run(nested_options)
     end
   end
